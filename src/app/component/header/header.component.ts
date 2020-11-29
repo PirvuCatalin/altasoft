fcf2c621decb19c2bfceee21f0cb221da0233caa
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-11-29 15:59:10 +0200

    switched href to routerLink and added header selected class

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  activeHeader: String = "home";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.subscribeToRouterChange();
   }
 
+  subscribeToRouterChange() {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url === "/" || val.url === "/home") {
+          this.activeHeader = "home";
+        } else if (val.url === "/about-us") {
+          this.activeHeader = "about-us";
+        } else {
+          this.activeHeader = "contact";
+        }
+      }
+    });
+  }
 }
