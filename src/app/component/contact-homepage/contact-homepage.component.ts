230c71bfd46e806f68ef69425f2675434e8ede3a
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-12-04 23:50:09 +0200

    simplified contact-us button code

--- src/app/component/contact-homepage/contact-homepage.component.ts
+++ src/app/component/contact-homepage/contact-homepage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-homepage',
@@ -8,12 +7,8 @@ import { Router } from '@angular/router';
 })
 export class ContactHomepageComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  contactUsButtonClicked() {
-    this.router.navigate(["/contact-us"]);
   }
 }
