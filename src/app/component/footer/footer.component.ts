f0f309b8b7cb52685abb730af91c66653c747873
PirvuCatalin <gabriel.pirvu0911@stud.acs.upb.ro>
2020-12-04 23:52:01 +0200

    added automatically updating footer copyright year

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  currentCopyrightYear : number = 2020; //fallback value
+   
   constructor() { }
 
   ngOnInit(): void {
+    this.currentCopyrightYear = new Date().getFullYear();
   }
 
 }
